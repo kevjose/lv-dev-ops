3acaaeb275ef9a7f7b5377581959fb830da5cb9c
Kevin Jose <kevin.jose@letsventure.com>
2016-03-14 13:10:32 +0530

    ngletteravatar

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-angular.module('MyApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', 'ui.router', 'satellizer', 'ui.bootstrap'])
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', 'ui.router', 'satellizer', 'ui.bootstrap', 'ngLetterAvatar'])
 
 .config(function ($stateProvider, $urlRouterProvider, $authProvider) {
   $stateProvider
