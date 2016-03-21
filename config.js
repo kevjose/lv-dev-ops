b5f1af90affcc5537ec9927167f3a028cd47de19
kevin <kevin.josev@gmail.com>
2016-03-21 20:15:46 +0530

    Update config.js
    remote mongo uri

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     TOKEN_SECRET: 'JWT Token Secret',
-    //MONGO_URI:'mongodb://kevin:opensesm@ds021299.mlab.com:21299/lv-dev-ops'
-    MONGO_URI:'mongodb://localhost:27017/lv-dev'
+    MONGO_URI:'mongodb://kevin:opensesm@ds021299.mlab.com:21299/lv-dev-ops'
+    //MONGO_URI:'mongodb://localhost:27017/lv-dev'
 };
