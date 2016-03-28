ef9d446c5e5fea73a5f38ea0dca139d7e19b9929
kevin <kevin.josev@gmail.com>
2016-03-28 07:20:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    TOKEN_SECRET: 'JWT Token Secret',
-    MONGO_URI:'mongodb://kevin:opensesm@ds021299.mlab.com:21299/lv-dev-ops'
-    //MONGO_URI:'mongodb://localhost:27017/lv-dev'
+  TOKEN_SECRET: 'Your Sceret Key',
+  MONGO_URI:'mongodb://localhost:27017/lv-dev'
 };
